ccaa3612c84006cd99af7f0f36355c442e0db93b
Oladimeji <oluyedis@gmail.com>
2022-05-21 09:55:47 +0100

    fixed preview box

--- main.js
+++ main.js
@@ -50,10 +50,10 @@ document.querySelectorAll('.food .slide').forEach(food =>{
    food.onclick = () =>{
       previewContainer.style.display = 'flex';
       let name = food.getAttribute('data-name');
-      previewBox.forEach(preview =>{
-         let target = preview.getAttribute('data-target');
+      previewBox.forEach(preveiw =>{
+         let target = preveiw.getAttribute('data-target');
          if(name == target){
-            preview.classList.add('active');
+            preveiw.classList.add('active');
          }
       });
    };
@@ -62,10 +62,7 @@ document.querySelectorAll('.food .slide').forEach(food =>{
 previewContainer.querySelector('#close-preview').onclick = () =>{
    previewContainer.style.display = 'none';
    previewBox.forEach(close =>{
-      // close.classList.remove('active');
-      previewContainer.classList.remove('close');
-      previewBox.classList.remove('close');
-
+      close.classList.remove('active');
    });
 };
 
